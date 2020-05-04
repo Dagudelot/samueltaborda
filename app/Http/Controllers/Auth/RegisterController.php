86bda99be27a8a79afe530e820d128cbf7109c9b
Daniel Agudelo <daniel.agudelo5169@unaula.edu.co>
2020-05-04 15:56:36 -0500

    changed return view of login and register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = 'client_pages.home';
 
     /**
      * Create a new controller instance.
