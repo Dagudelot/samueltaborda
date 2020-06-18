f85de42a72e417ce2d4b620fef17c275e572a9b7
Daniel Agudelo <daniel.agudelo5169@unaula.edu.co>
2020-06-17 19:54:39 -0500

    added meta description

--- resources/views/client_pages/home.blade.php
+++ resources/views/client_pages/home.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Psicólogo online | Blog de psicología | Psicólogo en Medellín"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
